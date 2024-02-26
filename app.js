44a0e224d951292056c1b8a107dcc73ed589f47f
TheJostler <josj@wjaag.com>
2024-02-26 14:19:59 +0100

    Update

--- app.js
+++ app.js
@@ -13,72 +13,67 @@ function renderSentence() {
     sentenceElement.textContent = sentence;
     }
 
-    function renderSelection() {
-        selectionElement.innerHTML = '';
-            words.forEach((word, index) => {
-                    const wordBtn = document.createElement('button');
-                            if(wordIndex == index) {
-                                            word = '-> ' + word;
-                                                    }
-                                                            wordBtn.textContent = word;
-                                                                    wordBtn.classList.add('btn', 'mb', 'w-full');
-                                                                            wordBtn.addEventListener('click', () => {
-                                                                                        sentence += word + ' ';
-                                                                                                    renderSentence();
-                                                                                                            });
-                                                                                                                    selectionElement.appendChild(wordBtn);
-                                                                                                                        });
-                                                                                                                        }
+function renderSelection() {
+	selectionElement.innerHTML = '';
+        words.forEach((word, index) => {
+        const wordBtn = document.createElement('button');
+	if(wordIndex == index) {
+		word = '-> ' + word;
+	}
+	wordBtn.textContent = word;
+	wordBtn.classList.add('btn', 'mb', 'w-full');
+	wordBtn.addEventListener('click', () => {
+		sentence += word + ' ';
+		renderSentence();
+	});
+	selectionElement.appendChild(wordBtn);
+	});
+}
+hammer.on('pandown', function() {
+	if(!blockGesture) {
+		if (wordIndex != 0) {
+			wordIndex--;
+        	} else {
+	        	wordIndex = words.length-1;
+		}
+			renderSelection();
+			blockGesture = true;
+		        setTimeout(function(){
+				blockGesture = false;
+	        	}, blockDelay);
+		}
+});
 
-                                                                                                                        hammer.on('pandown', function() {
-                                                                                                                                if(!blockGesture) {
-                                                                                                                                                if (wordIndex != 0) {
-                                                                                                                                                                wordIndex--;
-                                                                                                                                                                                } else {
-                                                                                                                                                                                                        wordIndex = words.length-1;
-                                                                                                                                                                                                                        }
+hammer.on('panup', function() {
+if(!blockGesture) {
+	if (wordIndex != words.length-1) {
+		wordIndex++;
+	} else {
+		wordIndex = 0;
+	}
+		renderSelection();
+		blockGesture = true;
+		setTimeout(function(){
+			blockGesture = false;
+		}, blockDelay);
+	}
+});
 
-                                                                                                                                                                                                                                        renderSelection();
+hammer.on('swiperight', function() {
+	if (words.length > 0) {
+        	const word = words[wordIndex]
+		sentence += word + ' ';
+                renderSentence();
+        }
+});
 
-                                                                                                                                                                                                                                                        blockGesture = true;
-                                                                                                                                                                                                                                                                        setTimeout(function(){
-                                                                                                                                                                                                                                                                                                blockGesture = false;
-                                                                                                                                                                                                                                                                                                                }, blockDelay);
-                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                        );
+hammer.on('swipeleft', function() {
+	if (sentence.trim() !== '') {
+		const lastWord = sentence.trim().split(' ').pop();
+		sentence = sentence.replace(lastWord + ' ', '');
+        	renderSentence();
+	}
+});
 
-                                                                                                                                                                                                                                                                                                                        hammer.on('panup', function() {
-                                                                                                                                                                                                                                                                                                                                if(!blockGesture) {
-                                                                                                                                                                                                                                                                                                                                        if (wordIndex != words.length-1) {
-                                                                                                                                                                                                                                                                                                                                                        wordIndex++;
-                                                                                                                                                                                                                                                                                                                                                                } else {
-                                                                                                                                                                                                                                                                                                                                                                                wordIndex = 0;
-                                                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                                                                renderSelection();
-
-                                                                                                                                                                                                                                                                                                                                                                                                        blockGesture = true;
-                                                                                                                                                                                                                                                                                                                                                                                                                setTimeout(function(){
-                                                                                                                                                                                                                                                                                                                                                                                                                                blockGesture = false;
-                                                                                                                                                                                                                                                                                                                                                                                                                                        }, blockDelay);
-                                                                                                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                                                                                                        });
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                        hammer.on('swiperight', function() {
-                                                                                                                                                                                                                                                                                                                                                                                                                                            if (words.length > 0) {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                    const word = words[wordIndex]
-                                                                                                                                                                                                                                                                                                                                                                                                                                                            sentence += word + ' ';
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                    renderSentence();
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                        });
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                        hammer.on('swipeleft', function() {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                            if (sentence.trim() !== '') {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    const lastWord = sentence.trim().split(' ').pop();
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            sentence = sentence.replace(lastWord + ' ', '');
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    renderSentence();
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        });
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        renderSentence();
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        renderSelection();
+renderSentence();
+renderSelection();
