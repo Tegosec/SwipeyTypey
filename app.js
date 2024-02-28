bd0fa024861d0e6bfa8d1ab6581682aa05fd785c
Paul HF <58356572+PaulHF7@users.noreply.github.com>
2024-02-28 00:58:19 +0000

    Capitalisation, bigger dictionary

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const hContainer = new Hammer(document.getElementById('selectionContainer'));
 const hSentence = new Hammer(sentenceElement);
 const hSelection = new Hammer(selectionElement);
 
-let sentence = '';
-let insertHistory = [];
-const dictionary = ['I', 'me', 'you', 'nurse', 'doctor', 'mum', 'dad', 'is', 'are', 'do', 'bed', 'tissue', 'mouth', 'head', 'leg', 'foot', 'arm', 'neck', 'eye', 'left', 'right', 'hurt', 'pain', 'itch', 'toilet', 'nose', 'can', 'my', 'drink', 'juice', 'food', 'feels', 'cold', 'in', 'hot', 'want', 'need', 'a', 'please', 'hand', 'finger', 'thirsty', 'hungry', 'chocolate', 'phone', 'charger', 'teddy', 'Cheddar', 'chest', 'wet', 'hair', 'brush', 'teeth', 'ask', 'tell', 'speak', 'to', 'the'];
+let sentence = '', insertHistory = [], dictionarySelect;
+const dictionary = ['I', 'me', 'you', 'nurse', 'doctor', 'mum', 'dad', 'is', 'are', 'do', 'bed', 'tissue', 'mouth', 'head', 'leg', 'foot', 'arm', 'neck', 'eye', 'left', 'right', 'hurt', 'pain', 'itch', 'toilet', 'nose', 'can', 'my', 'drink', 'juice', 'food', 'feels', 'cold', 'in', 'hot', 'want', 'need', 'a', 'please', 'hand', 'finger', 'thirsty', 'hungry', 'chocolate', 'phone', 'charger', 'teddy', 'Cheddar', 'chest', 'wet', 'hair', 'brush', 'teeth', 'ask', 'tell', 'speak', 'to', 'the', 'yes', 'no', 'what', 'when', 'where', 'how', 'who', 'why', 'pain', 'painkillers', 'painful', 'here', 'there', 'think', 'thought', 'can\'t', 'don\'t', 'uncomfortable', 'not', 'sleep', 'light', 'dark', 'on', 'off', 'ear', 'both', 'neither', 'they', 'tummy', 'fine', 'okay', 'good', 'bad', 'tingle', 'all', 'hope', 'hear', 'see', 'from', 'doing', 'come', 'coming', 'again', 'soon', 'thanks', 'thank', 'hello', 'goodbye', 'hi', 'hey', 'too', 'friend', 'cough', 'sneeze', 'vomit', 'sick', 'will', 'well', 'unwell', 'joints', 'blood', 'shave', 'dry'];
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const punctuation = [['Space',' '],['Full Stop', '.'],['Comma', ','],['Apostraphe', '\''],['Exclamation Mark', '!'],['Question Mark', '?'], ['Pound Sign', '£']];
 
@@ -16,34 +15,41 @@ function renderSentence() {
 }
 
 function renderSelectionDictionary() {
-        selectionElement.innerHTML = '';
-        dictionary.sort((a, b) => a.localeCompare(b, undefined, {sensitivity: 'base'})).forEach(word => {
-                const wordBtn = document.createElement('li');
-                wordBtn.textContent = word;
-                selectionElement.appendChild(wordBtn);
-        });
-        punctuation.forEach(renderSelectionPunctuation);
+        if(!dictionarySelect) {
+                selectionElement.innerHTML = '';
+                dictionary.sort((a, b) => a.localeCompare(b, undefined, {sensitivity: 'base'})).forEach(word => {
+                        const wordBtn = document.createElement('li');
+                        wordBtn.textContent = word;
+                        selectionElement.appendChild(wordBtn);
+                });
+                punctuation.forEach(renderSelectionPunctuation);
+                dictionarySelect = true;
+                selectionElement.scrollTo(0,0);
+        }
 }
 
 function renderSelectionAlphabet() {
-        selectionElement.innerHTML = '';
-        alphabet.forEach(letter => {
-                const wordBtn = document.createElement('li');
-                wordBtn.textContent = letter;
-                wordBtn.dataset.trimAfter = true;
-                selectionElement.appendChild(wordBtn);
-        });
-        punctuation.forEach(renderSelectionPunctuation);
+        if (dictionarySelect) {
+                selectionElement.innerHTML = '';
+                alphabet.forEach(letter => {
+                        const wordBtn = document.createElement('li');
+                        wordBtn.textContent = letter;
+                        selectionElement.appendChild(wordBtn);
+                });
+                punctuation.forEach(renderSelectionPunctuation);
+                dictionarySelect = false;
+                selectionElement.scrollTo(0,0);
+        }
 }
 
 function renderSelectionPunctuation(symbol, index) {
         const wordBtn = document.createElement('li');
         wordBtn.textContent = '(' + symbol[0] + ')';
         wordBtn.dataset.trueVal = symbol[1];
-        if(index < 6) 
+        if(index < 6) // All except (Pound)
                 wordBtn.dataset.trimBefore = true; // Remove spacing preceding this punctuation mark
-        if(index === 3 || index === 6) 
-                wordBtn.dataset.trimAfter = true; // Remove spacing following this punctuation mark
+        if(index !== 0 && index !== 3 && index !== 6) // (Space), (Apostraphe), (Pound)
+                wordBtn.dataset.spaceAfter = true; // Add spacing following this punctuation mark
         selectionElement.appendChild(wordBtn);
 }
 
@@ -57,11 +63,34 @@ hContainer.on('swipeleft', undo);
 function insertSelection() {
         const boxRect = selectionBox.getBoundingClientRect();
         const elements = document.elementsFromPoint(boxRect.x+5, boxRect.y+5);
-        if(elements[1].dataset.trimBefore) sentence = sentence.trim();
-        insertHistory.push(sentence.length);
-        sentence += elements[1].dataset.trueVal || elements[1].textContent;
-        if(!elements[1].dataset.trimAfter) sentence += ' ';
-        renderSentence();
+
+        //Check that a list item has been properly targetted
+        if(elements[1].tagName === 'LI') {
+
+                // Should there be a space?? Make sure there is / isn't
+                if(elements[1].dataset.trimBefore) {
+                        sentence = sentence.trim();
+                } else if (dictionarySelect && sentence.charAt(sentence.length-1) !== ' ') {
+                        sentence += ' ';
+                }
+
+                // Should we capitalise?
+                let toInsert = elements[1].dataset.trueVal || elements[1].textContent, punct;
+                if(insertHistory.length === 0 || (punct = sentence.charAt(sentence.length-2)) === '.' || punct === '!' || punct === '?') {
+                        toInsert = toInsert.charAt(0).toUpperCase() + toInsert.slice(1);
+                }
+
+                // Maintain insertion list for undo functionality
+                insertHistory.push(sentence.length);
+
+                // Add selection
+                sentence += toInsert;
+
+                // Insert space if required
+                if(elements[1].dataset.spaceAfter) sentence += ' ';
+
+                renderSentence();
+        }
 }
 
 function undo() {
